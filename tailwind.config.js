5b6328a3ce5ca6311a8c334151aff5f71d28a942
TomarVidhi04 <113514872+TomarVidhi04@users.noreply.github.com>
2022-09-18 23:35:31 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+
+      colors: {
+  
+        color1: "#DBEDF1", //header
+        mainColor: "#ffffff", //main white /
+        thirdColor: "#e6e7fd", // Home background
+        fourthColor: " #ee8260",
+        fifthColor: "#fafafe",
+        sixthColor: "#1e4755",
+       lightBlue :  "#E5F2FE",
+        SeventhColor: " #141414"  //cards text
+        
+      },
+      
+ 
+
+
+
+    },
+  },
+  plugins: [],
+}
